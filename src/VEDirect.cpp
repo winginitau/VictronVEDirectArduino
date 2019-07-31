5aeda90419bc5a9339cd97fbef7f27b618c062e6
svenp <sven.peitz@gmx.net>
2019-07-31 20:01:07 +0200

    mppt_only

---
+++ src/VEDirect.cpp
@@ -0,0 +1,116 @@
+/******************************************************************
+ VEDirect Arduino
+
+ Copyright 2018, 2019, Brendan McLearie
+ Distributed under MIT license - see LICENSE.txt
+
+ See README.md
+
+ File: VEDirect.cpp
+ - Implementation
+ Updates:
+ - 2019-07-14 See VEDirect.h
+******************************************************************/
+
+#include "VEDirect.h"
+
+VEDirect::VEDirect(HardwareSerial& port):
+	VESerial(port)
+	// Initialise the serial port that the
+	// VE.Direct device is connected to and
+	// store it for later use.
+{
+}
+
+VEDirect::~VEDirect() {
+	// virtual destructor
+}
+
+uint8_t VEDirect::begin() {
+	// Check connection the serial port
+	VESerial.begin(19200);
+	if (VESerial) {
+		delay(500);
+		if(VESerial.available()) {
+			VESerial.flush();
+			VESerial.end();
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int32_t VEDirect::read(uint8_t target) {
+	// Read VE.Direct lines from the serial port
+	// Search for the label specified by enum target
+	// Extract and return the corresponding value
+	// If value is "ON" return 1. If "OFF" return 0;
+
+	uint16_t loops = VED_MAX_READ_LOOPS;
+	uint8_t lines = VED_MAX_READ_LINES;
+	int32_t ret = 0;					// The value to be returned
+	char line[VED_LINE_SIZE] = "\0";	// Line buffer
+	uint8_t idx = 0;					// Line buffer index
+	char* label;
+	char* value_str;
+	int8_t b;							// byte read from the stream
+
+	VESerial.begin(VED_BAUD_RATE);
+
+	while (lines > 0) {
+		if (VESerial.available()) {
+			while (loops > 0) {
+				b = VESerial.read();
+				if ((b == -1) || (b == '\r')) { 	// Ignore '\r' and empty reads
+					loops--;
+				} else {
+					if (b == '\n') { 				// EOL
+						break;
+					} else {
+						if (idx < VED_LINE_SIZE) {
+							line[idx++] = b;		// Add it to the buffer
+						} else {
+							return 0;				// Buffer overrun
+						}
+					}
+				}
+			}
+			line[idx] = '\0';						// Terminate the string
+
+			// Line in buffer
+			if (target == VE_DUMP) {
+				// Diagnostic routine - just print to Serial0;
+				Serial.println(line);
+				// Continue on rather than break to reset for next line
+			}
+
+			label = strtok(line, "\t");
+			if (strcmp_P(label, ved_labels[target]) == 0) {
+				value_str = strtok(0, "\t");
+				if (value_str[0] == 'O') { 		//ON OFF type
+					if (value_str[1] == 'N') {
+						ret = 1;	// ON
+						break;
+					} else {
+						ret = 0;	// OFF
+						break;
+					}
+				} else {
+					sscanf(value_str, "%ld", &ret);
+					break;
+				}
+			} else {			// Line not of interest
+				lines--;
+				loops = VED_MAX_READ_LOOPS;
+				line[0] = '\0';
+				idx = 0;
+			}
+		}
+	}
+	return ret;
+}
+
+void VEDirect::copy_raw_to_serial0() {
+	read(VE_DUMP);
+}
+
---
+++ src/VEDirect.h
@@ -0,0 +1,84 @@
+/******************************************************************
+ VEDirect Arduino
+
+ Copyright 2018, 2019, Brendan McLearie
+ Distributed under MIT license - see LICENSE.txt
+
+ See README.md
+
+ File: VEDirect.h
+ - Class / enums / API
+
+ Updates:
+ - 2019-07-14:
+  	  - Rewrite of read - cleaner.
+  	  - Target labels extendible with enum and PROGMEM strings
+  	  - Retired copy_raw_to_serial0() code - use VE_DUMP on read
+  	  - Added some tunable parameters see #defines
+******************************************************************/
+
+#ifndef VEDIRECT_H_
+#define VEDIRECT_H_
+
+#include <Arduino.h>
+
+// Tunable parameters - defaults tested on mega2560 R3
+#define VED_LINE_SIZE 40		 // Seems to be plenty. VE.Direct protocol could change
+#define VED_MAX_LEBEL_SIZE 17	 // Max length of all labels of interest + '\0'. See ved_labels[]
+#define VED_MAX_READ_LOOPS 60000 // How many read loops to be considered a read time-out
+#define VED_MAX_READ_LINES 50	 // How many lines to read looking for a value
+								 // before giving up. Also determines lines for diag dump
+#define VED_BAUD_RATE 19200
+
+// Extend this and ved_labels[] for needed inclusions
+enum VE_DIRECT_DATA {
+	VE_DUMP = 0,
+	VE_FW,
+	VE_VOLTAGE,
+	VE_CURRENT,
+	VE_VOLTAGE_PV,
+	VE_POWER_PV,
+	VE_STATE,
+	VE_MPPT,
+	VE_ERROR,
+	VE_LOAD,
+	VE_YIELD_TOTAL,
+	VE_YIELD_TODAY,
+	VE_POWER_MAX_TODAY,
+	VE_YIELD_YESTERDAY,
+	VE_POWER_MAX_YESTERDAY,
+	VE_DAY_SEQUENCE_NUMBER,
+	VE_LAST_LABEL,
+};
+
+const char ved_labels[VE_LAST_LABEL][VED_MAX_LEBEL_SIZE] PROGMEM = {
+		"Dump",	// a string that won't match any label
+		"FW",
+		"V",
+		"I",
+		"VPV",
+		"PPV",
+		"CS",
+		"MPPT",
+		"ERR",
+		"LOAD",
+		"H19",
+		"H20",
+		"H21",
+		"H22",
+		"H23",
+		"HSDS",
+};
+
+class VEDirect {
+public:
+	VEDirect(HardwareSerial& port);
+	virtual ~VEDirect();
+	uint8_t begin();
+	int32_t read(uint8_t target);
+	void copy_raw_to_serial0(); // kept for backwards compatibility
+private:
+	HardwareSerial& VESerial;
+};
+
+#endif /* VEDIRECT_H_ */
