012841ff23919dc37d3589fb3255220677bac4f0
cterwilliger <chris@terwilliger.org>
2020-04-10 22:39:03 -0700

    Add files via upload

--- examples/ReadVEDirect/ReadVEDirect.ino
+++ examples/ReadVEDirect/ReadVEDirect.ino
@@ -8,6 +8,8 @@
 
  File: ReadVEDirect.ino / ReadVEDirect.cpp
  - Provides example use of the VEDirect library
+
+ 2020.04.10 - convert to SoftwareSerial
 ******************************************************************/
 
 #include "Arduino.h"
@@ -19,10 +21,10 @@ int32_t VE_soc, VE_power, VE_voltage, VE_current;
 uint8_t VE_alarm;
 
 // VEDirect instantiated with relevant serial object
-VEDirect myve(Serial3);
+VEDirect myve;
 
 void setup() {
-	Serial.begin(9600);		// Adjust as needed
+	Serial.begin(19200);		// Adjust as needed - DEBUG serial port
 }
 
 void loop() {
